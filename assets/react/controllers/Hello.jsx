e94ab4838dc287ef99cf67496902b328bc9664fb
manaka02 <toavina.apsesame2013@gmail.com>
2024-08-14 21:24:45 +0300

    initialize project

---
+++ assets/react/controllers/Hello.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const dashboard =  (props) => {
+    return <div>Hello {props.fullName}</div>;
+}
+
+export default dashboard;
---
+++ readme.md
@@ -0,0 +1,11 @@
+# projet de test Symfony - React Dashboard
+php 8.3
+npm
+Composer
+mysql 5
+
+git clone 
+
+composer install
+npm install
+npm run build
